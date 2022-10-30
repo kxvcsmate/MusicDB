f39789cabc7471f59352adf1745073d1db1669ee
Máté <Mt@DESKTOP-G0GFDSH>
2022-10-30 12:19:17 +0100

    Artist class added

--- C8N5NZ_HFT_2022231.Models/Artist.cs
+++ C8N5NZ_HFT_2022231.Models/Artist.cs
@@ -1,12 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace C8N5NZ_HFT_2022231.Models
 {
-    internal class Artist
+    public class Artist
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ArtistId { get; set; }
+        [Required]
+        [StringLength(240)]
+        public string Name { get; set; }
+
+        public Artist()
+        {
+
+        }
+        public Artist(string line)
+        {
+            string[] split = line.Split('#');
+            ArtistId = int.Parse(split[0]);
+            Name = split[1];
+        }
     }
 }
