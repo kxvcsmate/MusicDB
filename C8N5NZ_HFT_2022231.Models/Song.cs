79e94f1d895392b6e97b873688fd75cf8ae74545
Máté <Mt@DESKTOP-G0GFDSH>
2022-10-30 12:18:58 +0100

    Song class added

--- C8N5NZ_HFT_2022231.Models/Song.cs
+++ C8N5NZ_HFT_2022231.Models/Song.cs
@@ -1,12 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace C8N5NZ_HFT_2022231.Models
 {
-    internal class Song
+    public class Song
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int SongId { get; set; }
+        [StringLength(240)]
+        public string SongTitle { get; set; }
+        [Range(1, 10)]
+        public int Length { get; set; }
+        public int AlbumId { get; set; }
+        public Song()
+        {
+
+        }
+        public Song(string line)
+        {
+            string[] split = line.Split('#');
+            SongId = int.Parse(split[0]);
+            SongTitle = split[1];
+            Length = int.Parse(split[2]);
+            AlbumId = int.Parse(split[3]);
+        }
     }
 }
