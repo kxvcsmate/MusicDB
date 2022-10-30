9a4c0c48432fa23a1730ce4c7cb06ba8786dad76
Máté <Mt@DESKTOP-G0GFDSH>
2022-10-30 12:33:00 +0100

    restart

--- C8N5NZ_HFT_2022231.Repository/Database/MusicDbContext.cs
+++ C8N5NZ_HFT_2022231.Repository/Database/MusicDbContext.cs
@@ -1,12 +1,25 @@
-﻿using System;
+﻿using C8N5NZ_HFT_2022231.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection.Emit;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace C8N5NZ_HFT_2022231.Repository.Database
 {
-    internal class MusicDbContext
+    public class MusicDbContext
     {
+        public DbSet<Album> Albums { get; set; }
+        public DbSet<Artist> Artists { get; set; }
+        public DbSet<Song> Songs { get; set; }
+
+        public MusicDbContext()
+        {
+            this.Database.EnsureCreated();
+        }
+        
     }
 }
