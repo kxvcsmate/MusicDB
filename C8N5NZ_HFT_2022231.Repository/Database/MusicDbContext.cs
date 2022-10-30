9085691adf6a014f4713f4db0a759dcb6703897a
Máté <Mt@DESKTOP-G0GFDSH>
2022-10-30 12:38:22 +0100

    MusicDbContext class added

--- C8N5NZ_HFT_2022231.Repository/Database/MusicDbContext.cs
+++ C8N5NZ_HFT_2022231.Repository/Database/MusicDbContext.cs
@@ -2,15 +2,13 @@
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
-using System.Reflection.Emit;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace C8N5NZ_HFT_2022231.Repository.Database
 {
-    public class MusicDbContext
+    public class MusicDbContext : DbContext
     {
         public DbSet<Album> Albums { get; set; }
         public DbSet<Artist> Artists { get; set; }
@@ -20,6 +18,15 @@ namespace C8N5NZ_HFT_2022231.Repository.Database
         {
             this.Database.EnsureCreated();
         }
-        
+        protected override void OnConfiguring(DbContextOptionsBuilder builder)
+        {
+            if (!builder.IsConfigured)
+            {
+                string conn = @"Data Source=(LocalDB)\MSSQLLocalDB;
+                                AttachDbFilename=|DataDirectory|\.mdf;Integrated Security=True;MultipleActiveResultSets=true";
+                builder
+                .UseSqlServer(conn);
+            }
+        }
     }
 }
