24d1ea5509c4374c54f3c51de052acfb04a813c5
Máté <Mt@DESKTOP-G0GFDSH>
2022-11-05 13:42:42 +0100

    AlbumLogic class added

---
+++ C8N5NZ_HFT_2022231.Logic/AlbumLogic.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+using C8N5NZ_HFT_2022231.Models;
+using C8N5NZ_HFT_2022231.Repository;
+using C8N5NZ_HFT_2022231.Repository.Intefaces;
+
+namespace C8N5NZ_HFT_2022231.Logic
+{
+    public class AlbumLogic
+    {
+        IRepository<Album> repo;
+
+        public AlbumLogic(IRepository<Album> repo)
+        {
+            this.repo = repo;
+        }
+
+        public void Create(Album item)
+        {
+            this.repo.Create(item);
+        }
+
+        public void Delete(int id)
+        {
+            this.repo.Delete(id);
+        }
+
+        public Album Read(int id)
+        {
+            return this.repo.Read(id);
+        }
+
+        public IQueryable<Album> ReadAll()
+        {
+            return this.repo.ReadAll();
+        }
+
+        public void Update(Album item)
+        {
+            this.repo.Update(item);
+        }
+    }
+}
