f5dd0d4eb22893746bdcea2dedd580592ad3ebf8
Máté <Mt@DESKTOP-G0GFDSH>
2022-11-05 13:21:43 +0100

    Repository class added

--- C8N5NZ_HFT_2022231.Repository/Repository.cs
+++ C8N5NZ_HFT_2022231.Repository/Repository.cs
@@ -1,8 +1,37 @@
-﻿using System;
+﻿using C8N5NZ_HFT_2022231.Repository.Database;
+using System;
+using System.Linq;
 
 namespace C8N5NZ_HFT_2022231.Repository
 {
-    public class Repository
+    public abstract class Repository<T> : IRepository<T> where T : class
     {
+        protected MusicDbContext ctx;
+
+        protected Repository(MusicDbContext ctx)
+        {
+            this.ctx = ctx;
+        }
+
+        public void Create(T item)
+        {
+            ctx.Set<T>().Add(item);
+            ctx.SaveChanges();
+        }
+
+        public void Delete(int id)
+        {
+            ctx.Set<T>().Remove(Read(id));
+            ctx.SaveChanges();
+        }
+
+        public IQueryable<T> ReadAll()
+        {
+            return ctx.Set<T>();
+        }
+        public abstract T Read(int id);
+
+        public abstract void Update(T item);
+
     }
 }
