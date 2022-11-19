ed122e0f56ebe5e9a0da0b9999d91dcbcd8bcfb0
Máté <Mt@DESKTOP-G0GFDSH>
2022-11-19 18:24:16 +0100

    updated

--- C8N5NZ_HFT_2022231.Endpoint/Startup.cs
+++ C8N5NZ_HFT_2022231.Endpoint/Startup.cs
@@ -4,7 +4,9 @@ using C8N5NZ_HFT_2022231.Models;
 using C8N5NZ_HFT_2022231.Repository.Database;
 using C8N5NZ_HFT_2022231.Repository.Intefaces;
 using C8N5NZ_HFT_2022231.Repository.ModelRepositories;
+using Castle.Core.Configuration;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
@@ -20,8 +22,13 @@ namespace C8N5NZ_HFT_2022231.Endpoint
 {
     public class Startup
     {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+        public IConfiguration Configuration { get; }
+
         // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddTransient<MusicDbContext>();
@@ -47,16 +54,24 @@ namespace C8N5NZ_HFT_2022231.Endpoint
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "C8N5NZ_HFT_2022231.Endpoint v1"));
             }
+
+            app.UseExceptionHandler(c => c.Run(async context =>
+            {
+                var exception = context.Features.Get<IExceptionHandlerPathFeature>().Error;
+                var response = new { error = exception.Message };
+                await context.Response.WriteAsJsonAsync(response);
+            }));
 
             app.UseRouting();
 
+            app.UseAuthorization();
+
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapControllers();
             });
         }
     }
