5fc63f4dac77e7edf72e0714ab095107e9e82d0a
Máté <Mt@DESKTOP-G0GFDSH>
2022-11-27 13:16:27 +0100

    ConsoleMenu was added

--- C8N5NZ_HFT_2022231.Client/Program.cs
+++ C8N5NZ_HFT_2022231.Client/Program.cs
@@ -34,6 +34,47 @@ namespace C8N5NZ_HFT_2022231.Client
             var test6 = slogic.AlbumByLength();
             ;
 
+            RestService rest = new RestService("http://localhost:53770/", "music");
+            CrudService crud = new CrudService(rest);
+            NonCrudService nonCrud = new NonCrudService(rest);
+
+            var albumSubMenu = new ConsoleMenu(args, level: 1)
+                .Add("List", () => crud.List<Album>())
+                .Add("Create", () => crud.Create<Album>())
+                .Add("Delete", () => crud.Delete<Album>())
+                .Add("Update", () => crud.Update<Album>())
+                .Add("Exit", ConsoleMenu.Close);
+
+            var artistSubMenu = new ConsoleMenu(args, level: 1)
+                 .Add("List", () => crud.List<Artist>())
+                 .Add("Create", () => crud.Create<Artist>())
+                 .Add("Delete", () => crud.Delete<Artist>())
+                 .Add("Update", () => crud.Update<Artist>())
+                 .Add("Exit", ConsoleMenu.Close);
+
+            var songSubMenu = new ConsoleMenu(args, level: 1)
+                 .Add("List", () => crud.List<Song>())
+                 .Add("Create", () => crud.Create<Song>())
+                 .Add("Delete", () => crud.Delete<Song>())
+                 .Add("Update", () => crud.Update<Song>())
+                 .Add("Exit", ConsoleMenu.Close);
+
+            var crudSubMenu = new ConsoleMenu(args, level: 1)
+                .Add("Album with the most songs", () => nonCrud.AlbumWithTheMostSongs())
+                .Add("Artist with the longest aLbum", () => nonCrud.ArtistWithTheLongestALbum())
+                .Add("Artist with the highest rate aLbum", () => nonCrud.ArtistWithTheHighestRateALbum())
+                .Add("Average ratings by Artist", () => nonCrud.AVGRatingByArtist())
+                .Add("Albums by Length", () => nonCrud.AlbumByLength())
+                .Add("Exit", ConsoleMenu.Close);
+
+            var menu = new ConsoleMenu(args, level: 0)
+                .Add("Albums", () => albumSubMenu.Show())
+                .Add("Artists", () => artistSubMenu.Show())
+                .Add("Songs", () => songSubMenu.Show())
+                .Add("Non-CRUD", () => crudSubMenu.Show())
+                .Add("Exit", ConsoleMenu.Close);
+
+            menu.Show();
         }
     }
 }
--- C8N5NZ_HFT_2022231.Endpoint/Startup.cs
+++ C8N5NZ_HFT_2022231.Endpoint/Startup.cs
@@ -5,7 +5,9 @@ using C8N5NZ_HFT_2022231.Repository.Database;
 using C8N5NZ_HFT_2022231.Repository.Intefaces;
 using C8N5NZ_HFT_2022231.Repository.ModelRepositories;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -58,6 +60,13 @@ namespace C8N5NZ_HFT_2022231.Endpoint
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "C8N5NZ_HFT_2022231.Endpoint v1"));
             }
+
+            app.UseExceptionHandler(c => c.Run(async context =>
+            {
+                var exception = context.Features.Get<IExceptionHandlerPathFeature>().Error;
+                var response = new { error = exception.Message };
+                await context.Response.WriteAsJsonAsync(response);
+            }));
 
             app.UseRouting();
 
