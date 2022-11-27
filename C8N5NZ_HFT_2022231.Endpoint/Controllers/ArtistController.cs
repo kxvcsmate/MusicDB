82cb1d7ea2214b93b694bc5016fdb2658f3d296a
Máté <Mt@DESKTOP-G0GFDSH>
2022-11-27 12:54:54 +0100

    ArtistController was added

---
+++ C8N5NZ_HFT_2022231.Endpoint/Controllers/ArtistController.cs
@@ -0,0 +1,50 @@
+﻿using C8N5NZ_HFT_2022231.Logic.Interfaces;
+using C8N5NZ_HFT_2022231.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace C8N5NZ_HFT_2022231.Endpoint.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ArtistController : ControllerBase
+    {
+        IArtistLogic logic;
+        public ArtistController(IArtistLogic logic)
+        {
+            this.logic = logic;
+        }
+
+        [HttpGet]
+        public IEnumerable<Artist> ReadAll()
+        {
+            return this.logic.ReadAll();
+        }
+
+        [HttpGet("{id}")]
+        public Artist Read(int id)
+        {
+            return this.logic.Read(id);
+        }
+
+        [HttpPost]
+        public void Create([FromBody] Artist value)
+        {
+            this.logic.Create(value);
+        }
+
+        [HttpPut]
+        public void Put([FromBody] Artist value)
+        {
+            this.logic.Update(value);
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            this.logic.Delete(id);
+        }
+    }
+}
